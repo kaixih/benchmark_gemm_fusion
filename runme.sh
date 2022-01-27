0815efa2277d70a33886a529b97e5401cfe9c79c
Kaixi Hou <kaixih@nvidia.com>
2022-01-27 11:40:18 -0800

    Modify runme.sh for batch tests

--- runme.sh
+++ runme.sh
@@ -1,18 +1,23 @@
-CMD="./cudnn_v8_matmul_bias_gelu_fp16.out 0"
-#CMD="python tf_matmul_bias_gelu_fp16.py"
-#CMD="./cudnn_v8_matmul_bias_fp16.out 0"
-#CMD="python tf_matmul_bias_fp16.py"
-for ((m = 10; m < 15; m++))
+declare -a test_cmds=("./cudnn_v8_matmul_bias_gelu_fp16.out 0"
+                      "python tf_matmul_bias_gelu_fp16.py"
+                      "./cudnn_v8_matmul_bias_fp16.out 0"
+                      "python tf_matmul_bias_fp16.py")
+
+for test_cmd in "${test_cmds[@]}"
 do
-  for ((k = 10; k < 15; k++))
+  for ((m = 10; m < 15; m++))
   do
-    for ((n = 10; n < 15; n++))
+    for ((k = 10; k < 15; k++))
     do
-      M=$(( 2 ** $m ))
-      K=$(( 2 ** $k ))
-      N=$(( 2 ** $n ))
-      $CMD $M $K $N
+      for ((n = 10; n < 15; n++))
+      do
+        M=$(( 2 ** $m ))
+        K=$(( 2 ** $k ))
+        N=$(( 2 ** $n ))
+        $test_cmd $M $K $N
+      done
     done
   done
 done
+                      
 
