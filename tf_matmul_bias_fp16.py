3a20ab8552ecb287429f0b69604e76b5da488de4
kaixih <kaixih@nvidia.com>
2022-01-27 17:15:24 -0800

    Update python benchmark scripts

--- tf_matmul_bias_fp16.py
+++ tf_matmul_bias_fp16.py
@@ -1,41 +1,42 @@
-import argparse
 import tensorflow as tf
 import time
 
 from keras.layers import Dense
 from tensorflow.keras import mixed_precision
 
-parser = argparse.ArgumentParser(description='Benchmark Gemm+Bias')
-parser.add_argument('dims', metavar='N', type=int, nargs='*',
-                    help='integers representing M, K, N', default=[64, 32, 64])
-args = parser.parse_args()
-print("LOG >>> M, K, N:", args.dims)
+configs = []
+for M_e in range(10, 15):
+  for K_e in range(10, 15):
+    for N_e in range(10, 15):
+      configs.append((2**M_e, 2**K_e, 2**N_e));
 
-x = tf.random.normal(shape=(args.dims[0], args.dims[1]), dtype=tf.float16)
-w = tf.random.normal(shape=(args.dims[1], args.dims[2]), dtype=tf.float16)
-b = tf.random.normal(shape=(args.dims[2],), dtype=tf.float16)
-
-def matmal_bias(x, w, b):
-  y = tf.linalg.matmul(x, w)
-  z = tf.nn.bias_add(y, b)
-  return z
-
-def benchmark(repeats):
-  for i in range(repeats):
-    z = matmal_bias(x, w, b)
-  # Sync with a small D2H
-  out = tf.math.reduce_sum(z)
-  _ = out.numpy()
-
-burn_iters = 10
-time_iters = 50
-# Warmup
-benchmark(burn_iters)
-
-start = time.time()
-benchmark(time_iters)
-end = time.time()
-time_in_ms = (end - start) / time_iters * 1000
-
-print("LOG >>> Execution Time (ms):", time_in_ms)
+for M, K, N in configs:
+  print("LOG >>> M, K, N:", M, K, N)
+  x = tf.random.normal((M, K), dtype=tf.float16)
+  w = tf.random.normal((K, N), dtype=tf.float16)
+  b = tf.random.normal((N, ), dtype=tf.float16)
+  
+  def matmal_bias(x, w, b):
+    y = tf.linalg.matmul(x, w)
+    z = tf.nn.bias_add(y, b)
+    return z
+  
+  def benchmark(repeats):
+    for i in range(repeats):
+      z = matmal_bias(x, w, b)
+    # Sync with a small D2H
+    out = tf.math.reduce_sum(z)
+    _ = out.numpy()
+  
+  burn_iters = 10
+  time_iters = 50
+  # Warmup
+  benchmark(burn_iters)
+  
+  start = time.time()
+  benchmark(time_iters)
+  end = time.time()
+  time_in_ms = (end - start) / time_iters * 1000
+  
+  print("LOG >>> Execution Time (ms):", time_in_ms)
 
--- tf_matmul_bias_gelu_fp16.py
+++ tf_matmul_bias_gelu_fp16.py
@@ -1,44 +1,45 @@
-import argparse
 import tensorflow as tf
 import time
 
 from keras.layers import Dense
 from tensorflow.keras import mixed_precision
 
-parser = argparse.ArgumentParser(description='Benchmark Gemm+Bias')
-parser.add_argument('dims', metavar='N', type=int, nargs='*',
-                    help='integers representing M, K, N', default=[64, 32, 64])
-args = parser.parse_args()
-print("LOG >>> M, K, N:", args.dims)
+configs = []
+for M_e in range(10, 15):
+  for K_e in range(10, 15):
+    for N_e in range(10, 15):
+      configs.append((2**M_e, 2**K_e, 2**N_e));
 
-x = tf.random.normal(shape=(args.dims[0], args.dims[1]), dtype=tf.float16)
-w = tf.random.normal(shape=(args.dims[1], args.dims[2]), dtype=tf.float16)
-b = tf.random.normal(shape=(args.dims[2],), dtype=tf.float16)
-
-# We use the graph mode (tf.function), since the gelu graph is optimized.
-@tf.function
-def matmal_bias_gelu(x, w, b):
-  y = tf.linalg.matmul(x, w)
-  z = tf.nn.bias_add(y, b)
-  z = tf.keras.activations.gelu(z)
-  return z
-
-def benchmark(repeats):
-  for i in range(repeats):
-    z = matmal_bias_gelu(x, w, b)
-  # Sync with a small D2H
-  out = tf.math.reduce_sum(z)
-  _ = out.numpy()
-
-burn_iters = 10
-time_iters = 50
-# Warmup
-benchmark(burn_iters)
-
-start = time.time()
-benchmark(time_iters)
-end = time.time()
-time_in_ms = (end - start) / time_iters * 1000
-
-print("LOG >>> Execution Time (ms):", time_in_ms)
+for M, K, N in configs:
+  print("LOG >>> M, K, N:", M, K, N)
+  x = tf.random.normal((M, K), dtype=tf.float16)
+  w = tf.random.normal((K, N), dtype=tf.float16)
+  b = tf.random.normal((N, ), dtype=tf.float16)
+  
+  # We use the graph mode (tf.function), since the gelu graph is optimized.
+  @tf.function
+  def matmal_bias_gelu(x, w, b):
+    y = tf.linalg.matmul(x, w)
+    z = tf.nn.bias_add(y, b)
+    z = tf.keras.activations.gelu(z)
+    return z
+  
+  def benchmark(repeats):
+    for i in range(repeats):
+      z = matmal_bias_gelu(x, w, b)
+    # Sync with a small D2H
+    out = tf.math.reduce_sum(z)
+    _ = out.numpy()
+  
+  burn_iters = 10
+  time_iters = 50
+  # Warmup
+  benchmark(burn_iters)
+  
+  start = time.time()
+  benchmark(time_iters)
+  end = time.time()
+  time_in_ms = (end - start) / time_iters * 1000
+  
+  print("LOG >>> Execution Time (ms):", time_in_ms)
 
